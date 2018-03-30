d06300292c057a99531edb2f43cc1ca5501e7ab2
dravig <misutinv@gmail.com>
2018-03-30 13:57:51 +0300

    Added hashes of passwords

--- environments/production/modules/mysql-core/manifests/init.pp
+++ environments/production/modules/mysql-core/manifests/init.pp
@@ -1,13 +1,13 @@
 class { '::mysql::server':
-   root_password           => '123',
+   root_password           => '*BB46AE99452DD250C5A791F16CDD15AC36F1993A',#523
    remove_default_accounts => true,
    override_options        => {
-    mysqld => { bind-address => '0.0.0.0'} #Allow remote connections
+   mysqld => { bind-address => '0.0.0.0'} #Allow remote connections
   }
  }
 
  mysql::db { 'app_db':
   user => 'dbuser',
-  password => '123',
+  password => '*23AE809DDACAF96AF0FD78ED04B6A265E05AA257',
   host => '%',
   }
