a4eddd64074dd3c40548295641bb2e0b26ba8a00
dravig <misutinv@gmail.com>
2018-04-11 18:29:55 +0300

    Bash v2

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,5 +1,18 @@
 node db{ 
 include 'core'
+file { '/tmp/replica.sh':
+
+ source => "puppet:///modules/core/replica.sh",
+ ensure => 'file',
+ owner => 'root',
+ group => 'root',
+ mode  => '0700',
+ notify => Exec['exec_script'],
+ }
+ exec { 'exec_script':
+  command => "/bin/bash -c '/tmp/replica.sh'",
+ }
+
 }
 
 node db-slave{
